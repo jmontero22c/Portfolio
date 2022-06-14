6ef1ed04a3ad8d9ea4533b9c5f9c7c1f79ad9261
jmontero22c <jesusda71.2@gmail.com>
2022-06-13 22:56:49 -0500

    Finish the switch between sections

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,5 +1,5 @@
 const sections = document.querySelectorAll('.section')
-const sectBtns = document.querySelectorAll('.crontrolls')
+const sectBtns = document.querySelectorAll('.controlls')
 const sectBtn = document.querySelectorAll('.control')
 const allSections = document.querySelector('.main-content')
 
@@ -11,6 +11,25 @@ function pageTransition(){
             this.className += ' active-btn'
         });
     }
+
+    allSections.addEventListener('click',(e)=>{
+        const id = e.target.dataset.id
+        if(id){
+            //Remove selected from the other botons
+            sectBtns.forEach((btn)=>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+            
+            //hide other sections
+            sections.forEach((sec)=>{
+                sec.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active')
+        }
+
+    })
 }
 pageTransition();
-console.log(sectBtn[0].className);
